625832e8a0a6db99a006e2cb3542036107211a9d
smosmth <smosmth@gmail.com>
2024-05-17 09:01:35 +0500

    1st change

---
+++ app/src/main/java/com/example/thenewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.thenewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "fcb16e35e0cb4eb287d939f2191d6ccd"
+        const val BASE_URL = "https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
