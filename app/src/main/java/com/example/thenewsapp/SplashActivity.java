07b49f76cddb12d11fd662f5a3c91c373d02deff
smosmth <smosmth@gmail.com>
2024-05-17 09:42:44 +0500

    Splash screen add

---
+++ app/src/main/java/com/example/thenewsapp/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.thenewsapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.thenewsapp.ui.NewsActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    public static int SPLASH_TIMER = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, NewsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
